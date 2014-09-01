d0473d1189d631f13d4b84c9140006e5a5959266
gogotanaka <qlli.illb@gmail.com>
2014-09-01 16:17:59 -0700

    Fix something

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :news
-  belongs_to :comment
-end
--- app/models/relation_vote.rb
+++
@@ -1,4 +0,0 @@
-class RelationVote < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :news
-end
